4f986864b2988d585ff46ae93198b42c1108c8e2
KAA76 <kaa76study@gmail.com>
2025-04-17 14:59:14 +0800

    0. Что такое тестирование [#159431 #88501]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,13 +2,11 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float result = value / 70;
-        return result;
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
-        float result = value / 60; /* формула перевода рублей в доллары. */
-        return result;
+        return value / 60;
     }
 
     public static void main(String[] args) {
