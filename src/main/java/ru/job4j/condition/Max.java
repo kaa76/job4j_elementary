6c1e85f49766c1eab9393283850dfa7173aad74d
KAA76 <kaa76study@gmail.com>
2025-05-07 16:50:17 +0800

    4.2. Тернарное сравнение [#189 #88435]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        return left > right ? left : right;
+    }
+
+    public static int summation(int first, int second) {
+        return first + second;
+    }
+}
