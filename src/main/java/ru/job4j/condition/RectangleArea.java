bec3eb49a584f9a492f55c7d37b4801304d144d3
KAA76 <kaa76study@gmail.com>
2025-05-12 16:46:08 +0800

    4.5. Отладка программы в IDEA [#159464 #88426]

--- src/main/java/ru/job4j/condition/RectangleArea.java
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -1,12 +1,10 @@
 package ru.job4j.condition;
 
 public class RectangleArea {
-    public static double square(double p, double k) {
-        return Math.pow(p / (2 * (k + 1)), 2) * k;
-    }
-
-    public static void main(String[] args) {
-        double result = RectangleArea.square(7, 1);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + result);
+    public static double square(int p, double k) {
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double s = l * h;
+        return s;
     }
 }
--- src/test/java/ru/job4j/condition/RectangleAreaTest.java
+++ src/test/java/ru/job4j/condition/RectangleAreaTest.java
@@ -1,34 +1,25 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 class RectangleAreaTest {
-
     @Test
     void whenP6K2Square2() {
         int expected = 2;
         int p = 6;
         double k = 2;
-        double output = RectangleArea.square(p, k);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        double out = RectangleArea.square(p, k);
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 
     @Test
-    void whenP6K0Square0() {
-        int expected = 0;
-        int p = 6;
-        double k = 0;
-        double output = RectangleArea.square(p, k);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
-    }
-
-    @Test
-    void whenP7K1Square3Dot06() {
-        double expected = 3.06;
-        int p = 7;
+    void whenP4K1Square1() {
+        int expected = 1;
+        int p = 4;
         double k = 1;
-        double output = RectangleArea.square(p, k);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        double out = RectangleArea.square(p, k);
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 }
